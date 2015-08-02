0233b685d4d9444f10db3130195bf5e59c0bfd8b
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-08-02 19:57:01 +0200

    Modernise JavaScript usage
    Use strict, no defer but proper on-ready, global functions via window,
    put vars where JS actually interprets them at.

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,66 @@
-var $info = $('#info');
-var $current = $('#current');
-var $player = $('#player');
-var $playlist = $('#playlist');
-var $counter = $('#counter');
+'use strict';
 
-var timer, song_len, song_current;
+$(function() {
+  var $info = $('#info');
+  var $current = $('#current');
+  var $player = $('#player');
+  var $playlist = $('#playlist');
+  var $counter = $('#counter');
 
-function updateBar() {
-  var percent = Math.floor(((song_len - song_current) / song_len) * 100);
-  $('#progressbar').width(percent + '%');
+  var timer, song_len, song_current;
+  var io = new RocketIO().connect();
 
-  var date = new Date(null);
-  date.setSeconds(song_current);
-  var offset = 14, len = 5;
-  if (song_current > 3600) {
-    offset = 11;
-    len = 8;
+  window.updateBar = function() {
+    var percent = Math.floor(((song_len - song_current) / song_len) * 100);
+    var date = new Date(null);
+    var offset = 14, len = 5;
+
+    $('#progressbar').width(percent + '%');
+
+    date.setSeconds(song_current);
+    if (song_current > 3600) {
+      offset = 11;
+      len = 8;
+    }
+    $('#timer').html(date.toISOString().substr(offset, len));
+
+    song_current -= 1;
+    timer = setTimeout(updateBar, 1000);
+  };
+
+  window.playBar = function(state, elapsed, time) {
+    clearTimeout(timer);
+    if (!state) {
+      return;
+    }
+    song_current = time - elapsed;
+    song_len = time;
+    updateBar();
+  };
+
+  window.reloadPlayer = function() {
+    $player.load();
+  };
+
+  window.toggleInfo = function() {
+    $info.toggle('slow');
   }
-  $('#timer').html(date.toISOString().substr(offset, len));
 
-  song_current -= 1;
-  timer = setTimeout(updateBar, 1000);
-};
-
-function playBar(state, elapsed, time) {
-  clearTimeout(timer);
-  if (!state) {
-    return;
+  if ($player.length > 0) {
+    $player[0].autoplay = false;
   }
-  song_current = time - elapsed;
-  song_len = time;
-  updateBar();
-};
 
-function reloadPlayer() {
-  $player.load();
-};
-
-function toggleInfo() {
-  $info.toggle('slow');
-}
-
-if ($player.length > 0) {
-  $player[0].autoplay = false;
-}
-
-var io = new RocketIO().connect();
-
-io.on('current', function(data) {
-  $current.load('/ajax/current');
-});
-
-io.on('playlist', function(data) {
-  $playlist.load('/ajax/playlist');
-});
-
-if ($counter.length > 0) {
-  io.on('counter', function(data) {
-    $counter.html(data).effect("highlight", 250);
+  io.on('current', function(data) {
+    $current.load('/ajax/current');
   });
-}
+
+  io.on('playlist', function(data) {
+    $playlist.load('/ajax/playlist');
+  });
+
+  if ($counter.length > 0) {
+    io.on('counter', function(data) {
+      $counter.html(data).effect("highlight", 250);
+    });
+  }
+});
